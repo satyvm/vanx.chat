061452d36aa82ff77d8178f481198638e8201016
Satyam <75127014+satyvm@users.noreply.github.com>
2025-10-16 21:54:55 +0530

    updated: eslint for next

--- apps/web/eslint.config.js
+++ apps/web/eslint.config.js
@@ -1,4 +1,4 @@
 import { nextJsConfig } from "@vanx/eslint-config/next-js"
 
-/** @type {import("eslint").Linter.Config} */
+/** @type {import("eslint").Linter.Config[]} */
 export default nextJsConfig
